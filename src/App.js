2ca470ef834a136f607339b14a554ba5298b191f
dbgb <dbayliss.mail@gmail.com>
2018-12-17 13:18:49 +0000

    Remove react-fittext dependency

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import Clock from 'react-live-clock';
-import ReactFitText from 'react-fittext';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
       <div className="App">
         <header className="App-header">
-          <ReactFitText compressor={0.3}>
-            <h1>
-              <Clock format="HH:mm:ss" ticking={true} interval={1000} />
-            </h1>
-          </ReactFitText>
+          <div>
+            <h1><Clock class="clock" format="HH:mm:ss" ticking={true} interval={1000} /></h1>
+          </div>
           <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
