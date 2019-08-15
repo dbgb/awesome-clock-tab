19ee52408b34e1521b6c88470efe8dc52a1b381b
dbgb <dbayliss.mail@gmail.com>
2019-08-15 14:21:06 +0100

    Add console info message

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+console.info(`Source code available @ https://github.com/dbgb/awesome-clock-tab`);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
