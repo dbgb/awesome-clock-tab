4d2038642af02c383f3df15d6734359150f2e3c6
dbgb <dbayliss.mail@gmail.com>
2019-08-15 13:36:28 +0100

    Add awesome and zoidberg logos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Clock from "react-live-clock";
-import logo from "./logo.svg";
+import logo from "./img/logo.svg";
 import "./App.css";
 
 class App extends Component {
